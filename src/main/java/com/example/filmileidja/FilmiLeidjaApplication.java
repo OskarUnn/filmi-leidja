28bf9362059d979929cce7b752f9e4a2650e1544
Oskar U <oskar.unn@gmail.com>
2024-03-05 21:38:53 +0200

    Spring Boot'i projekti ülesseadmine

---
+++ src/main/java/com/example/filmileidja/FilmiLeidjaApplication.java
@@ -0,0 +1,12 @@
+package com.example.filmileidja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmiLeidjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmiLeidjaApplication.class, args);
+	}
+}
