461d1577d198f1002b3ae76b1c2e9751295fa282
Oskar U <oskar.unn@gmail.com>
2024-03-12 19:21:43 +0200

    Parem istmete valimise leht

--- src/main/resources/static/js/valikoht.js
+++ src/main/resources/static/js/valikoht.js
@@ -6,12 +6,10 @@ Vue.component('istekoht', {
     created() {
         this.valitud = false;
         app.istekohad[this.rida + "-" + this.iste] = this;
-        console.log(app.istekohad);
     },
     methods: {
         pressed() {
-            if (!this.valitud && !this.hõivatud) {
-                // app.istekohad[(this.rida) + "-" + (this.iste+1)].valitud = true;
+            if (!this.hõivatud) {
                 app.valiUuedIstmed(this.rida, this.iste);
             }
         }
@@ -21,14 +19,15 @@ let app = new Vue({
     el: '#app',
     data: {
         seanss: null,
-        istekohad: {}
+        istekohad: {},
+        valitudKohad: [],
     },
     mounted() {
         this.küsiAndmed();
     },
     methods: {
         küsiAndmed() {
-            axios.get('/api/v1/seanss/'+ seanssID)
+            axios.get('/api/v1/seanss/' + seanssID)
                 .then(response => {
                     this.seanss = response.data;
                 })
@@ -37,11 +36,24 @@ let app = new Vue({
                 });
         },
         valiUuedIstmed(rida, iste) {
-            for (let key in this.istekohad) {
-                let istekoht = this.istekohad[key];
+            // for (let key in this.istekohad) {
+            //     let istekoht = this.istekohad[key];
+            //     istekoht.valitud = false;
+            // }
+            let istekoht = this.istekohad[rida+"-"+iste];
+            if (istekoht.valitud) {
+                this.valitudKohad = this.valitudKohad.filter(item => item !== istekoht);
                 istekoht.valitud = false;
+            } else if (this.valitudKohad.length < kohtadeArv) {
+                this.valitudKohad.push(istekoht);
+                istekoht.valitud = true;
             }
-            this.istekohad[rida + "-" + iste].valitud = true;
         }
     }
 });
+
+function kuupäev(dateTimeString) { // ChatGPT abil loodud funktsioon
+    const date = new Date(dateTimeString);
+    const options = { weekday: 'long', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', timeZone: 'Europe/Tallinn' };
+    return date.toLocaleDateString('et-EE', options);
+}
