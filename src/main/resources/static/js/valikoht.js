6511742fd6e4f6a92f8398a51475be0c41c44d0c
Oskar U <40840287+OskarUnn@users.noreply.github.com>
2024-03-12 14:09:14 +0200

    Uus funktsioon istmete valimiseks

--- src/main/resources/static/js/valikoht.js
+++ src/main/resources/static/js/valikoht.js
@@ -1,15 +1,18 @@
 Vue.component('istekoht', {
     props: ['rida', 'iste', 'hõivatud', 'valitud'],
     template: `
-            <button v-on:click="valiIste()" :class="{'bg-red-500': hõivatud, 'bg-green-500': valitud}" class="bg-gray-300 rounded shadow-2xl w-10 h-10 m-1"></button>
+            <button v-on:click="pressed()" :class="{'bg-red-500': hõivatud, 'bg-green-500': valitud}" class="bg-gray-300 rounded shadow-2xl w-10 h-10 m-1"></button>
         `,
     created() {
         this.valitud = false;
+        app.istekohad[this.rida + "-" + this.iste] = this;
+        console.log(app.istekohad);
     },
     methods: {
-        valiIste() {
+        pressed() {
             if (!this.valitud && !this.hõivatud) {
-                this.valitud = !this.valitud;
+                // app.istekohad[(this.rida) + "-" + (this.iste+1)].valitud = true;
+                app.valiUuedIstmed(this.rida, this.iste);
             }
         }
     }
@@ -17,7 +20,8 @@ Vue.component('istekoht', {
 let app = new Vue({
     el: '#app',
     data: {
-        seanss: null
+        seanss: null,
+        istekohad: {}
     },
     mounted() {
         this.küsiAndmed();
@@ -31,6 +35,13 @@ let app = new Vue({
                 .catch(error => {
                     console.error('Error fetching data:', error);
                 });
+        },
+        valiUuedIstmed(rida, iste) {
+            for (let key in this.istekohad) {
+                let istekoht = this.istekohad[key];
+                istekoht.valitud = false;
+            }
+            this.istekohad[rida + "-" + iste].valitud = true;
         }
     }
 });
