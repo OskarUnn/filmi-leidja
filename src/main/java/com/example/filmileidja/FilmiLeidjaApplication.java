40951134db67a7f6cb5ab698ab5057b18592f4fd
Oskar U <40840287+OskarUnn@users.noreply.github.com>
2024-03-13 21:45:09 +0200

    Kindlusta, et filmid lisatakse andmebaasi enne kinokava koostamist

--- src/main/java/com/example/filmileidja/FilmiLeidjaApplication.java
+++ src/main/java/com/example/filmileidja/FilmiLeidjaApplication.java
@@ -2,6 +2,8 @@ package com.example.filmileidja;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -26,71 +28,26 @@ public class FilmiLeidjaApplication {
 	}
 
 	@Bean
-	CommandLineRunner lisaFilmid(FilmRepository filmRepository) {
-		return args -> {
-			if (filmRepository.count() > 0) {
-				return;
-			}
-
-			ArrayList<Film> filmid = new ArrayList<>();
-			filmid.add(new Film("Elu ja armastus", Duration.ofMinutes(118), "Draama", "Elu_ja_armastus.jpg"));
-			filmid.add(new Film("Kung Fu Panda 4", Duration.ofMinutes(94), "Märul, Seiklus, Animatsioon", "KungFuPanda4.jpg"));
-			filmid.add(new Film("Düün: teine osa", Duration.ofMinutes(166), "Ulme, Seiklus", "Düün2.jpg"));
-			filmid.add(new Film("Pardid!", Duration.ofMinutes(92), "Komöödia, Seiklus, Animatsioon", "Pardid.jpg"));
-			filmid.add(new Film("Mahajäänud", Duration.ofMinutes(133), "Komöödia, Draama", "Mahajäänud.jpg"));
-
-			filmRepository.insert(filmid);
-
-			logger.info("Filmid lisatud andmebaasi");
-		};
-	}
-
-	@Bean
-	@DependsOn("lisaFilmid")
-	CommandLineRunner koostaKinokava(FilmRepository filmRepository, SeanssRepository seanssRepository) {
-		return args -> {
-			if (seanssRepository.count() > 0) {
-				return;
-			}
-			if (filmRepository.count() <= 0) {
-				logger.error("Kinokava üritati luua, aga andmebaasis pole ühtegi filmi mida näidata");
-				return;
-			}
-
-			List<Film> filmid = filmRepository.findAll();
-
-			// kino päevane avamis ja sulgemis kellaaeg
-			LocalTime avamisAeg = LocalTime.of(9, 0); // 09:00
-			LocalTime sulgemisAeg = LocalTime.of(21, 30); // 21:30
-
-			// Koosta ühe kuu kinokava
-			ArrayList<Seanss> kinokava = new ArrayList<>();
-			LocalDateTime kinokavaLõpp = LocalDate.now().atTime(sulgemisAeg).plusMonths(1);
-			LocalDateTime viimaseSeanssiLõpp = LocalDateTime.now().with(avamisAeg).minusMinutes(20);
-			Random random = new Random();
-			LocalDateTime päevaSulgemine = viimaseSeanssiLõpp.with(sulgemisAeg);
-
-			while (viimaseSeanssiLõpp.isBefore(kinokavaLõpp)) {
-
-				LocalDateTime seanssiAlgus = viimaseSeanssiLõpp.plusMinutes(15); // jäta >15 minutit seansside vahele
-				seanssiAlgus = seanssiAlgus.plusMinutes(5 - (seanssiAlgus.getMinute() % 5)); // ümarda seanssi algus 5 minuti vahemikku
-
-				// liiguta seanssi algus järgmisesse päeva kui eelmine lõppes peale sulgemist
-				if (seanssiAlgus.isAfter(päevaSulgemine)) {
-					päevaSulgemine = päevaSulgemine.plusDays(1);
-					seanssiAlgus = päevaSulgemine.with(avamisAeg);
+	ApplicationRunner lisaFilmid(FilmRepository filmRepository, SeanssRepository seanssRepository, KinokavaService koostaKinokavaService) {
+		return new ApplicationRunner() {
+			@Override
+			public void run(ApplicationArguments args) throws Exception {
+				if (filmRepository.count() > 0) {
+					return;
 				}
 
-				Film suvalineFilm = filmid.get(random.nextInt(filmid.size()));
+				ArrayList<Film> filmid = new ArrayList<>();
+				filmid.add(new Film("Elu ja armastus", Duration.ofMinutes(118), "Draama", "Elu_ja_armastus.jpg"));
+				filmid.add(new Film("Kung Fu Panda 4", Duration.ofMinutes(94), "Märul, Seiklus, Animatsioon", "KungFuPanda4.jpg"));
+				filmid.add(new Film("Düün: teine osa", Duration.ofMinutes(166), "Ulme, Seiklus", "Düün2.jpg"));
+				filmid.add(new Film("Pardid!", Duration.ofMinutes(92), "Komöödia, Seiklus, Animatsioon", "Pardid.jpg"));
+				filmid.add(new Film("Mahajäänud", Duration.ofMinutes(133), "Komöödia, Draama", "Mahajäänud.jpg"));
 
-				viimaseSeanssiLõpp = seanssiAlgus.plus(suvalineFilm.getPikkus());
+				filmRepository.insert(filmid);
+				logger.info("Filmid lisatud andmebaasi");
 
-				Seanss uusSeanss = new Seanss(suvalineFilm, seanssiAlgus);
-				kinokava.add(uusSeanss);
+				koostaKinokavaService.koostaKinokava();
 			}
-			seanssRepository.insert(kinokava);
-
-			logger.info("Kinokava koostatud");
 		};
 	}
 }
---
+++ src/main/java/com/example/filmileidja/KinokavaService.java
@@ -0,0 +1,70 @@
+package com.example.filmileidja;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Service
+public class KinokavaService {
+    private static final Logger logger = LoggerFactory.getLogger(KinokavaService.class);
+
+    private final FilmRepository filmRepository;
+    private final SeanssRepository seanssRepository;
+
+    public KinokavaService(FilmRepository filmRepository, SeanssRepository seanssRepository) {
+        this.filmRepository = filmRepository;
+        this.seanssRepository = seanssRepository;
+    }
+
+    public void koostaKinokava() {
+        if (seanssRepository.count() > 0) {
+            return;
+        }
+        if (filmRepository.count() <= 0) {
+            logger.error("Kinokava üritati luua, aga andmebaasis pole ühtegi filmi mida näidata");
+            return;
+        }
+
+        List<Film> filmid = filmRepository.findAll();
+
+        // kino päevane avamis ja sulgemis kellaaeg
+        LocalTime avamisAeg = LocalTime.of(9, 0); // 09:00
+        LocalTime sulgemisAeg = LocalTime.of(21, 30); // 21:30
+
+        // Koosta ühe kuu kinokava
+        ArrayList<Seanss> kinokava = new ArrayList<>();
+        LocalDateTime kinokavaLõpp = LocalDate.now().atTime(sulgemisAeg).plusMonths(1);
+        LocalDateTime viimaseSeanssiLõpp = LocalDateTime.now().with(avamisAeg).minusMinutes(20);
+        Random random = new Random();
+        LocalDateTime päevaSulgemine = viimaseSeanssiLõpp.with(sulgemisAeg);
+
+        while (viimaseSeanssiLõpp.isBefore(kinokavaLõpp)) {
+
+            LocalDateTime seanssiAlgus = viimaseSeanssiLõpp.plusMinutes(15); // jäta >15 minutit seansside vahele
+            seanssiAlgus = seanssiAlgus.plusMinutes(5 - (seanssiAlgus.getMinute() % 5)); // ümarda seanssi algus 5 minuti vahemikku
+
+            // liiguta seanssi algus järgmisesse päeva kui eelmine lõppes peale sulgemist
+            if (seanssiAlgus.isAfter(päevaSulgemine)) {
+                päevaSulgemine = päevaSulgemine.plusDays(1);
+                seanssiAlgus = päevaSulgemine.with(avamisAeg);
+            }
+
+            Film suvalineFilm = filmid.get(random.nextInt(filmid.size()));
+
+            viimaseSeanssiLõpp = seanssiAlgus.plus(suvalineFilm.getPikkus());
+
+            Seanss uusSeanss = new Seanss(suvalineFilm, seanssiAlgus);
+            kinokava.add(uusSeanss);
+        }
+        seanssRepository.insert(kinokava);
+
+        logger.info("Kinokava koostatud");
+    }
+}
