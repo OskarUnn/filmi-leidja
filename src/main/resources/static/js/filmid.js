fd61e114c27ac0046b044a3a6d85446939d8108f
Oskar U <40840287+OskarUnn@users.noreply.github.com>
2024-03-15 12:41:36 +0200

    Filmi vanusepiirangu näitamine

--- src/main/resources/static/js/filmid.js
+++ src/main/resources/static/js/filmid.js
@@ -2,17 +2,20 @@ Vue.component('filmi-kaart', {
     props: ['film'],
     template: `
         <div class="flex bg-gray-200 shadow-outline rounded w-full">
-            <div class="flex-shrink-0 w-full w-1/2">
-              <img :src="film.poster" alt="Image" class="h-full w-full object-cover rounded-l">
+            <div class="relative flex-shrink-0 w-full w-1/2">
+                <img :src="film.poster" alt="Image" class="h-full w-full object-cover rounded-l">
+                <div class="absolute bottom-0 right-0 p-2 bg-black bg-opacity-50 text-white rounded-bl">
+                    <p class="text-lg">{{ film.vanusepiirang }}</p>
+                </div>
             </div>
             <div class="flex flex-row justify-between p-4 w-1/2">
                 <div class="flex flex-col space-y-4">
-                  <h2 class="font-bold w-40 text-3xl">{{ film.pealkiri }}</h2>
-                  <p class="text-left text-2xl font-semibold">{{ film.žanrid.join(", ") }}</p>
+                    <h2 class="font-bold w-40 text-3xl">{{ film.pealkiri }}</h2>
+                    <p class="text-left text-2xl font-semibold">{{ film.žanrid.join(", ") }}</p>
                 </div>
             </div>
-          </div>
-        `,
+        </div>
+    `,
 });
 let app = new Vue({
     el: '#app',
--- src/main/resources/static/js/kinokava.js
+++ src/main/resources/static/js/kinokava.js
@@ -35,29 +35,32 @@ Vue.component('seanssi-kaart', {
         };
     },
     template: `
-          <div class="flex bg-gray-200 shadow-outline rounded min-w-min">
-            <div class="flex-shrink-0 h-full w-1/3">
-              <img :src="seanss.film.poster" alt="Image" class="h-full w-full object-cover rounded-l">
+        <div class="flex bg-gray-200 shadow-outline rounded min-w-min">
+            <div class="relative flex-shrink-0 h-full w-1/3">
+                <img :src="seanss.film.poster" alt="Image" class="h-full w-full object-cover rounded-l">
+                <div class="absolute bottom-0 right-0 p-2 bg-black bg-opacity-50 text-white rounded-bl">
+                    <p class="text-xs">{{ seanss.film.vanusepiirang }}</p>
+                </div>
             </div>
             <div class="flex flex-row justify-between p-4 w-2/3">
                 <div class="flex flex-col">
-                  <h2 class="font-bold w-40 text-2xl">{{ seanss.film.pealkiri }}</h2>
-                  <p class="text-left text-xl font-semibold">{{ seanss.film.žanrid.join(", ") }}</p>
+                    <h2 class="font-bold w-40 text-2xl">{{ seanss.film.pealkiri }}</h2>
+                    <p class="text-left text-xl font-semibold">{{ seanss.film.žanrid.join(", ") }}</p>
                 </div>
-              <div class="flex flex-col">
-                <p class="text-left text-xl font-semibold">{{ kuupäev(seanss.algus) }}</p>
-                <div class="flex flex-row items-center">
-                    <p class="text-xl mr-2">Kohtade arv:</p>
-                    <input type="number" min="0" v-model="valitudKohti" class="mt-2 border border-gray-400 rounded px-2 py-1 w-16">
+                <div class="flex flex-col">
+                    <p class="text-left text-xl font-semibold">{{ kuupäev(seanss.algus) }}</p>
+                    <div class="flex flex-row items-center">
+                        <p class="text-xl mr-2">Kohtade arv:</p>
+                        <input type="number" min="0" v-model="valitudKohti" class="mt-2 border border-gray-400 rounded px-2 py-1 w-16">
+                    </div>
                 </div>
-              </div>
-              <button v-on:click="valiSeanss(seanss.id, valitudKohti)"
-                      class="self-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Vali seanss
-              </button>
+                <button v-on:click="valiSeanss(seanss.id, valitudKohti)"
+                        class="self-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Vali seanss
+                </button>
             </div>
-          </div>
-        `,
+        </div>
+    `,
     methods: {
         valiSeanss(id, kohtadeArv) {
             if (kohtadeArv <= 0) {
