84167976b67a2d4917108605363ea1d2b9d24ddc
Oskar U <40840287+OskarUnn@users.noreply.github.com>
2024-03-15 16:30:08 +0200

    Rohkem filme

--- src/main/java/com/example/filmileidja/FilmiLeidjaApplication.java
+++ src/main/java/com/example/filmileidja/FilmiLeidjaApplication.java
@@ -35,6 +35,11 @@ public class FilmiLeidjaApplication {
 				filmid.add(new Film("Düün: teine osa", Duration.ofMinutes(166), "Ulme, Seiklus", "Düün2.jpg", "MS-12"));
 				filmid.add(new Film("Pardid!", Duration.ofMinutes(92), "Komöödia, Seiklus, Animatsioon", "Pardid.jpg", "PERE"));
 				filmid.add(new Film("Mahajäänud", Duration.ofMinutes(133), "Komöödia, Draama", "Mahajäänud.jpg", "K-12"));
+				filmid.add(new Film("Halastamatu maa", Duration.ofMinutes(113), "Märul, Triller, Sõda", "HalastamatuMaa.jpg", "K-12"));
+				filmid.add(new Film("Biwa järve 8 nägu", Duration.ofMinutes(126), "Draama", "Biwa.jpg", "K-12"));
+				filmid.add(new Film("Oppenheimer", Duration.ofMinutes(180), "Biograafia, Ajalooline draama", "Oppenheimer.jpg", "K-12"));
+				filmid.add(new Film("Kass ja koer: põgenemine", Duration.ofMinutes(87), "Komöödia, Seiklus", "Cat&Dog.jpg", "MS-6"));
+				filmid.add(new Film("Poiss ja haigur", Duration.ofMinutes(124), "Draama, Seiklus, Animatsioon", "BoyHeron.jpg", "MS-6"));
 
 				filmRepository.insert(filmid);
 				logger.info("Filmid lisatud andmebaasi");
